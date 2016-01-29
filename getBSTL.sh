b099d37865173855173f6013c4a52c81254b6c84
matt <l3acon@gmail.com>
2016-01-29 23:53:01 +0000

    updating loader scripts

--- getBSTL.sh
+++ getBSTL.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-wget http://www.fnarfbargle.com/bst/bstl/Latest/bstl.osx.zip
-unzip bstl.osx.zip
+if [[ `uname -s` = "Linux" ]] ; then
+  wget http://www.fnarfbargle.com/bst/bstl/Latest/bstl.linux.zip
+  unzip bstl.linux.zip
+  rm bstl.linux.zip
+
+elif [[ `uname -s` = "Darwin" ]] ; then
+  wget http://www.fnarfbargle.com/bst/bstl/Latest/bstl.osx.zip
+  unzip bstl.osx.zip
+  rm bstl.osx.zip
+else
+  echo "Platform not supported"
+fi
---
+++ getPropLoader.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget http://www.orrtech.us/propeller-load-rpi-3.zip 
+unzip propeller-load-rpi-3.zip
+rm propeller-load-rpi-3.zip
+
